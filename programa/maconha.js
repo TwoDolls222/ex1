bd4ae575b65f5a1acb078e28081286481f252559
TwoDolls222 <twodollsnc@gmail.com>
2025-04-28 16:17:33 -0300

    ex1pt1

---
+++ programa/maconha.js
@@ -0,0 +1,8 @@
+console.log(20 > 5 || 5 < 10);
+const media = 7;
+var nota = prompt("Digite a nota do aluno"); 
+
+if(nota == 10){console.log("Parabens aluno voce passou!"); alert("Parabens aluno voce passou!");}
+else if(nota >= media){console.log("o aluno passou"); alert("o aluno passou");}
+else if(nota <= media){console.log("recuperacao"); alert("recuperacao");}
+else if(nota <= 4){console.log("Aluno reprovado"); alert("Aluno reprovado");}
